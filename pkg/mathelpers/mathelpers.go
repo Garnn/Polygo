887aa923f76e5303b523394c4f5ed9fb9bc6eb9b
Garnn <bawalilko@gmail.com>
2022-06-21 08:36:00 +0200

    created mathelpers package

---
+++ pkg/mathelpers/mathelpers.go
@@ -0,0 +1,11 @@
+package mathelpers
+
+func bruteGetDivisors(i int) (divisors []int) {
+	for divisor := 1; divisor <= i/2; divisor += 1 {
+		if i%divisor == 0 {
+			divisors = append(divisors, divisor)
+		}
+	}
+
+	return divisors
+}
