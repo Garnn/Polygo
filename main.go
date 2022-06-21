0ec20e71b5c3c6d5652364cf27aac1e698e283a1
Garnn <bawalilko@gmail.com>
2022-06-21 19:12:39 +0200

    commented code, made it look nicer

--- main.go
+++ main.go
@@ -11,14 +11,20 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+
+	//Ask for a polynomial, try to read it, and panic if it can't be read
 	fmt.Print("Enter a polynomial: ")
 	text, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatalf("Text reader encountered an error: %v", err)
 	}
+
+	//Parse the polynomial into a more workable format thanks to Gucio's Parser, panic if it fails somehow
 	result, err := equation.Parse(text)
 	if err != nil {
 		log.Fatalf("Equation parser encountered an error: %v", err)
 	}
+
+	//Check if polynomials can convert back into strings
 	fmt.Println(result)
 }
