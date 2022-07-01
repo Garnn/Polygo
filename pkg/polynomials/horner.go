e549ec13e691c2454aaf2d4f02af0871966c54f2
Garnn <bawalilko@gmail.com>
2022-07-01 22:16:44 +0200

    Started implementing findPotentialSolutions func in horner

--- pkg/polynomials/horner.go
+++ pkg/polynomials/horner.go
@@ -1,5 +1,17 @@
 package polynomials
 
-func FindPotentialSolutions(equation Equation) {
+import (
+	"github.com/garnn/Polygo/pkg/mathelpers"
+)
 
+func findPotentialSolutions(equation Equation) (solutions []int) {
+	divisors_1 := mathelpers.BruteGetDivisors(equation.Monomials[len(equation.Monomials)-1].Coefficient)
+	divisors_2 := mathelpers.BruteGetDivisors(equation.Monomials[0].Coefficient)
+	solutions = divisors_1
+	for _, div1 := range divisors_1 {
+		for _, div2 := range divisors_2 {
+			solutions = append(solutions, div1/div2)
+		}
+	}
+	return solutions
 }
